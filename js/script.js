c3e9bfc22d980c5c7e3f9bfc8812a8053563e5c0
DavideGila <davide.gila@gmail.com>
2023-10-24 16:20:47 +0200

    made a list with grades > 70 and id > 120 using filter

--- js/script.js
+++ js/script.js
@@ -44,36 +44,46 @@ const students = [
     {
         id: '213',
         name: 'Marco della Rovere',
-        grades: '78'
+        grades: 78
     },
     {
         id: '110',
         name: 'Paola Cortellessa',
-        grades: '96'
+        grades: 96
     },
     {
         id: '250',
         name: 'Andrea Mantegna',
-        grades: '48'
+        grades: 48
     },
     {
         id: '145',
         name: 'Gaia Borromini',
-        grades: '74'
+        grades: 74
     },
     {
         id: '196',
         name: 'Luigi Grimaldello',
-        grades: '68'
+        grades: 68
     },
     {
         id: '102',
         name: 'Piero della Francesca',
-        grades: '50'
+        grades: 50
     },
     {
         id: '120',
         name: 'Francesca da Polenta',
-        grades: '84'
+        grades: 84
     },
 ]
+
+let grades = students.filter((element) => {
+    return element.grades > 70;
+})
+console.log(grades);
+
+let grades2 = students.filter((element) => {
+    return element.grades > 70 && element.id > '120';
+})
+console.log(grades2);
